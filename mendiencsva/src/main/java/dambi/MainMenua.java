977e566f7c3c84deb1826705bb608dcbb1d46491
Julen Peláez <pelaez.julen@uni.eus>
2023-09-20 12:11:44 +0200

    mendiak bukatuta

--- mendiencsva/src/main/java/dambi/MainMenua.java
+++ mendiencsva/src/main/java/dambi/MainMenua.java
@@ -1,14 +1,14 @@
 package dambi;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainMenua {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sn = new Scanner(System.in);
         boolean salir = false;
         int opcion;
@@ -19,7 +19,7 @@ public class MainMenua {
             System.out.println("=================");
             System.out.println("1. Mendien zerrenda ikusi (taula formatuan)");
             System.out.println("2. Mendirik altuena bistaratu");
-            System.out.println("3. Mendiak esportatu (Araba.csv, Bizkaia.csv, Gipuzkoa.csv)");
+            System.out.println("3. Mendiak esportatu (Araba.csv, Bizkaia.csv, Gipuzkoa.csv, Nafarroa.csv)");
             System.out.println("4. ...");
             System.out.println("5. Irten");
             System.out.print("Aukeratu: ");
@@ -27,12 +27,13 @@ public class MainMenua {
 
             switch (opcion) {
                 case 1:
-                    met1();
+                    zerrendaikusi();
                     break;
                 case 2:
                     mendialtuena();
                     break;
                 case 3:
+                    mendiakesportatu();
                     System.out.println("Has seleccionado la opcion 3");
                     break;
                 case 4:
@@ -47,7 +48,7 @@ public class MainMenua {
         }
     }
 
-    public static void met1() {
+    public static void zerrendaikusi() {
         ArrayList<Mendia> mendiak = new ArrayList<>();
         mendiak = cargarmendiak();
 
@@ -57,7 +58,8 @@ public class MainMenua {
         }
     }
 
-    /** Metodo honek....
+    /**
+     * Metodo honek....
      * 
      */
     public static void mendialtuena() {
@@ -76,6 +78,34 @@ public class MainMenua {
                 System.out.println(j);
             }
 
+        }
+    }
+
+    public static void mendiakesportatu() throws IOException {
+        ArrayList<Mendia> mendiak = cargarmendiak();
+
+        FileOutputStream outGipuzkoa = new FileOutputStream("Gipuzkoa.csv");
+        FileOutputStream outBizkaia = new FileOutputStream("Bizkaia.csv");
+        FileOutputStream outAraba = new FileOutputStream("Araba.csv");
+        FileOutputStream outNafarroa = new FileOutputStream("Nafarroa.csv");
+
+        for (Mendia m : mendiak) {
+            if (m.getProbintzia().equals("Gipuzkoa")) {
+                String linea = m.getIzena() + ";" + m.getAltuera() + ";" + m.getProbintzia() + "\n";
+                outGipuzkoa.write(linea.getBytes());
+            }
+            if (m.getProbintzia().equals("Bizkaia")) {
+                String linea = m.getIzena() + ";" + m.getAltuera() + ";" + m.getProbintzia() + "\n";
+                outBizkaia.write(linea.getBytes());
+            }
+            if (m.getProbintzia().equals("Araba")) {
+                String linea = m.getIzena() + ";" + m.getAltuera() + ";" + m.getProbintzia() + "\n";
+                outAraba.write(linea.getBytes());
+            }
+            if (m.getProbintzia().equals("Nafarroa")) {
+                String linea = m.getIzena() + ";" + m.getAltuera() + ";" + m.getProbintzia() + "\n";
+                outNafarroa.write(linea.getBytes());
+            }
         }
     }
 

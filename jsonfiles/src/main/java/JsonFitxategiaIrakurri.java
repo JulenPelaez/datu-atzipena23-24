faaddd96090395dbfce7ab84991f59ef654696e9
Julen Peláez <pelaez.julen@uni.eus>
2023-10-11 12:43:53 +0200

    avance

---
+++ jsonfiles/src/main/java/JsonFitxategiaIrakurri.java
@@ -0,0 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import javax.json.Json;
+import javax.json.JsonReader;
+import javax.json.JsonStructure;
+
+public class JsonFitxategiaIrakurri {
+    public static void main(String[] args) throws FileNotFoundException {
+        JsonReader reader = Json.createReader(new FileReader("irteera.json"));
+        JsonStructure jsonst = reader.read();
+        System.out.println(jsonst);
+    }
+
+}
